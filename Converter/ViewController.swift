cf4c63fe38c5e477c101830d81ca1cda6955384c
Администратор <vladimirmelnikov@vladimirs-imac.local>
2016-06-20 14:27:18 +0300

    Initial Commit

---
+++ Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by Владимир Мельников on 20/06/16.
+//  Copyright © 2016 Владимир Мельников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
