c00d7a3599f3b2e6c2e1193025d55e0acd004095
Vladimirmlnkv <vlmlnkv@gmail.com>
2016-06-25 17:52:52 +0300

    Fixed bug with button title

--- Converter/DisclosureButton.swift
+++ Converter/DisclosureButton.swift
@@ -10,18 +10,11 @@ import UIKit
 
 @IBDesignable
 class DisclosureButton: UIButton {
-    
-    override func imageRectForContentRect(contentRect:CGRect) -> CGRect {
-        var imageFrame = super.imageRectForContentRect(contentRect)
-        imageFrame.origin.x = CGRectGetMaxX(super.titleRectForContentRect(contentRect)) - CGRectGetWidth(imageFrame)
-        return imageFrame
-    }
-    
-    override func titleRectForContentRect(contentRect:CGRect) -> CGRect {
-        var titleFrame = super.titleRectForContentRect(contentRect)
-        if (self.currentImage != nil) {
-            titleFrame.origin.x = CGRectGetMinX(super.imageRectForContentRect(contentRect))
-        }
-        return titleFrame
+
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        transform = CGAffineTransformMakeScale(-1.0, 1.0);
+        titleLabel!.transform = CGAffineTransformMakeScale(-1.0, 1.0);
+        imageView!.transform = CGAffineTransformMakeScale(-1.0, 1.0);
     }
 }
